5df100d1398b70dcf84be78117054d17a94db314
joaqib <jo.ib.pm@gmail.com>
2016-03-21 12:13:30 +0100

    added Task form within Goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,7 @@
 class GoalsController < ApplicationController
 
+ before_action :set_goal, only: [:show, :edit, :update, :destroy]
+
   def index
     @goals = Goal.all
     @tasks = Task.all
@@ -14,27 +16,31 @@ class GoalsController < ApplicationController
   def create
     @goal = Goal.new(goal_params)
 
-    if @goal.save
-      flash[:notice] = "Goal created succesfully"
-      redirect_to action: :index
-    else
-      render "new"
+    respond_to do |format|
+      if @goal.save
+        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.json { render :show, status: :created, location: @goal }
+      else
+        format.html { render :new }
+        format.json { render json: @goal.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def archive
     Goal.find(params[:id]).update_attributes(done: true)
-    redirect_to goals_path
+    redirect_to goals_path, notice: "Goal successfully archive"
   end
 
   def show
-    @goal = Goal.find(params[:id])
   end
 
   private
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
   def goal_params
     params.require(:goal).permit(:name, tasks_attributes: [:name])
   end
-
-
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,29 @@
 class TasksController < ApplicationController
+  before_action :set_goal
+  before_action :set_item, except: [:create]
+
+  def create
+    @task = @goal.tasks.create(task_params)
+    @task.save
+    redirect_to @goal
+  end
+
+  private
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
+  def set_task
+    @task = @goal.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
+
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @goal, notice: "Task completed"
+  end
 end
+ 
